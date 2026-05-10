776cbd3c595213e96cf021fa597c53413db159ae
Rafnix Guzman <rafnixg@gmail.com>
2026-05-09 23:27:27 -0500

    chore: remove build/ and .cxx/ from git tracking

--- app/build/intermediates/cxx/Debug/5b4j213u/logs/arm64-v8a/build_command_synth-lib.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\rafni\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\projectos\\midi-bt-controller\\app\\.cxx\\Debug\\5b4j213u\\arm64-v8a" ^
-  synth-lib
